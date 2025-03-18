76e2709d641778473bc3e4d9985eccc3d7d289d6
Man Jesus <awosojiemmanuel2019@gmail.com>
2025-03-18 22:24:10 +0100

    Updated

--- src/components/Nutrition/Nutrition.jsx
+++ src/components/Nutrition/Nutrition.jsx
@@ -10,19 +10,19 @@ function Nutrition() {
           addition fillings.
         </p>
         <div>
-          <div className="border-b border-black/25 flex justify-between px-28 py-3">
+          <div className="border-b border-black/25 flex justify-between px-10 md:px-28 py-3">
             <p>Calories</p>
             <p className="text-[#714539] font-bold">277kcal</p>
           </div>
-          <div className="border-b border-black/25 flex justify-between px-28 py-3">
+          <div className="border-b border-black/25 flex justify-between px-10 md:px-28 py-3">
             <p>Carbs</p>
             <p className="text-[#714539] font-bold">0g</p>
           </div>
-          <div className="border-b border-black/25 flex justify-between px-28 py-3">
+          <div className="border-b border-black/25 flex justify-between px-10 md:px-28 py-3">
             <p>Protein</p>
             <p className="text-[#714539] font-bold">20g</p>
           </div>
-          <div className="border-b border-black/25 flex justify-between px-28 py-3">
+          <div className="border-b border-black/25 flex justify-between px-10 md:px-28 py-3">
             <p>Fat</p>
             <p className="text-[#714539] font-bold">22g</p>
           </div>
